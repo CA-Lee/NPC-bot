a73599c6ba73961d8fa5fbeea7f517ea586ab65c
CA-Lee <email@calee.tw>
2021-02-02 15:52:13 +0000

    feat(text_handler): keyword match

--- app/event_handler.py
+++ app/event_handler.py
@@ -14,7 +14,18 @@ def handle_text_message(event: MessageEvent):
     Echo the same message
     """
 
+    actual_answer = "(正解)"
+    reply_message = """夢想，可以天花亂墜。
+理想，是我們一步一腳印，
+踩出來的坎坷道路。"""
+    if "思考" in event.message.text:
+        reply_message = actual_answer
+    if "行動" in event.message.text:
+        reply_message = actual_answer
+    if "突破" in event.message.text:
+        reply_message = actual_answer
+
     line_bot_api = LineBotApi(LINE_CHANNEL_TOKEN)
     line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(event.message.text)
+        event.reply_token, TextSendMessage(reply_message)
     )
